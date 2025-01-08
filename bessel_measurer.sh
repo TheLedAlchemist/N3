df8740fe7f25ae1df4a0bdcc4015a9f43d19631f
Matthew Gill <matthew.gill467@gmail.com>
2025-01-08 09:17:07 -0500

    [Implementation] Wrote a working bessel automation script

--- bessel_measurer.sh
+++ bessel_measurer.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+#BSUB -n 10
+#BSUB -W 120
+#BSUB -J besselTest
+#BSUB -o stdout.%J
+#BSUB -e stderr.%J
+
 # Define the number of seeds and the number of parallel jobs
-NUM_SEEDS=1
-PARALLEL_JOBS=20
+NUM_SEEDS=100
+MAX_LAMBDA=10.00
 PLATFORM="cpu"
 
 # Define the paths to the Python scripts
@@ -11,6 +17,9 @@ BESSEL_STANDARD_SCRIPT="./scripts/bessel_standard.py"
 
 # Define the base output directory
 OUTPUT_BASE_DIR="./output"
+
+export PYTHONPATH=$(pwd)/src:$PYTHONPATH
+echo "Set the python path to: $PYTHONPATH"
 
 # Function to run a Python script with a given seed and output directory
 run_script() {
@@ -28,8 +37,12 @@ static_output_dir="${OUTPUT_BASE_DIR}/BESSEL/"
 standard_output_dir="${OUTPUT_BASE_DIR}/BESSEL/"
 
 for seed in $(seq 0 $((NUM_SEEDS - 1))); do
-    for i in $(seq 0.01 0.01 1.1); do
+    echo "Begin training on seed $seed"
+    for i in $(seq 0.01 0.01 $MAX_LAMBDA); do
 	run_script $BESSEL_STATIC_SCRIPT $seed $static_output_dir $i
 	run_script $BESSEL_STANDARD_SCRIPT $seed $standard_output_dir $i
     done
+    echo "Finished training seed $seed"
 done
+
+echo "JOB COMPLETE"
