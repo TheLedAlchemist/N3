d6bc25bcb37e0bfe35c6e8aefb89bbdc32713bd6
Matthew Gill <msgill@ncsu.edu>
2025-01-23 14:18:55 -0500

    [Implementation] Parallelized the bessel measurement script

--- bessel_measurer.sh
+++ bessel_measurer.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 #BSUB -n 100
-#BSUB -W 9000
+#BSUB -W 900
 #BSUB -J besselTest
 #BSUB -o stdout.%J
 #BSUB -e stderr.%J
 
 # Define the number of seeds and the number of parallel jobs
-NUM_SEEDS=30
+NUM_SEEDS=50
 PLATFORM="cpu"
+MAX_PARALLEL=100
 
 # Define the paths to the Python scripts
 BESSEL_STATIC_SCRIPT="./scripts/bessel_static.py"
@@ -35,23 +36,28 @@ run_script() {
 static_output_dir="${OUTPUT_BASE_DIR}/BESSEL"
 standard_output_dir="${OUTPUT_BASE_DIR}/BESSEL"
 
-for seed in $(seq 0 2 $((NUM_SEEDS - 1))); do
+for i in $(seq -2.0 0.01 2.0); do
     echo "Begin training on seed $seed"
     
     static_out_seed="$static_output_dir/SEED_${seed}/"
     standard_out_seed="$standard_output_dir/SEED_${seed}/"
 
+    # Define lambda logarithmically in terms of i
+    size_influence=$(awk "BEGIN {print 10^($i)}")
+
     # Scan through lambda logarithmically from 1e-2, 1e-1.99, ... , 0, ... , 1e+1.99, 1e+2
-    for i in $(seq -2.0 0.01 2.0); do
-	size_influence=$(awk "BEGIN {print 10^($i)}")
-	nextseed=$(expr $seed + 1)
-	run_script $BESSEL_STATIC_SCRIPT $seed "${static_output_dir}/SEED_${seed}" $size_influence &
-	run_script $BESSEL_STANDARD_SCRIPT $seed "${standard_output_dir}/SEED_${seed}" $size_influence &
-	run_script $BESSEL_STATIC_SCRIPT $nextseed "${static_output_dir}/SEED_${nextseed}" $size_influence &
-	run_script $BESSEL_STANDARD_SCRIPT $nextseed "${sdandard_output_dir}/SEED_${nextseed}" $size_influence
+    for seed in $(seq 0 $((NUM_SEEDS - 1)); do
+        run_script $BESSEL_STATIC_SCRIPT $seed "${static_output_dir}/SEED_${seed}" $size_influence &
+        run_script $BESSEL_STANDARD_SCRIPT $seed "${standard_output_dir}/SEED_${seed}" $size_influence &
+
+        if (( $jobs -r | wc -l ) >= MAX_PARALLEL); then
+            wait -n
+        fi
     done
 
+    wait
     echo "Finished training seed $seed"
 done
 
-echo "JOB COMPLETE"
+wait
+echo "All jobs COMPLETE"
