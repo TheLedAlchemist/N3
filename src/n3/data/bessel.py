69baf1db41475d0a2a74e53ff441803bcf945fb9
Matthew Gill <msgill@ncsu.edu>
2025-03-20 19:29:31 -0400

    Implemented simple bessel function

--- src/n3/data/bessel.py
+++ src/n3/data/bessel.py
@@ -11,6 +11,11 @@ def bessel(x):
     """
     return jv(0, x) + jv(1, x) + jv(2, x)
 
+def simple_bessel(x):
+    """
+    Bessel function assigned to aux-neuron algorithm
+    """
+    return -265.83 + 267.5 * jv(0, 0.15 + 0.05*x)
 
 def generate_data(
     n_samples: int = 1000,
@@ -20,6 +25,23 @@ def generate_data(
 ):
     x = np.linspace(-2 * np.pi, 2 * np.pi, n_samples).reshape(-1, 1)
     y = bessel(x).reshape(-1, 1)
+
+    x_scaled, y_scaled = scaler.fit_transform(x), scaler.fit_transform(y)
+
+    x_train, x_test, y_train, y_test = train_test_split(
+        x_scaled, y_scaled, test_size=test_size, random_state=seed
+    )
+
+    return jnp.array(x_train), jnp.array(x_test), jnp.array(y_train), jnp.array(y_test)
+
+def generate_simple_data(
+    n_samples: int = 1000,
+    test_size: float = 0.2,
+    scaler: MinMaxScaler = MinMaxScaler(feature_range=(-1, 1)),
+    seed: int = 0,
+):
+    x = np.linspace(-1, 1, n_samples).reshape(-1, 1)
+    y = simple_bessel(x).reshape(-1, 1)
 
     x_scaled, y_scaled = scaler.fit_transform(x), scaler.fit_transform(y)
 
